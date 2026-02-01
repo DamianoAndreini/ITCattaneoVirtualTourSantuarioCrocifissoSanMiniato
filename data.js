fd915108ac914e4d2070b4b9ddb74b0c45ae327c
DamianoAndreini <damiano.andreini72@gmail.com>
2026-02-01 23:15:08 +0100

    Caricata la cartella completa del tour Crocifisso
    H trasferito tutti i files contenuti nella cartella scaricata da Marzipano contenente il virtual tour del Crocifisso di San Miniato, verso quella di GITHUB (in documenti)

---
+++ data.js
@@ -0,0 +1,109 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-santuario-del-ss-crocifisso",
+      "name": "Santuario del SS. Crocifisso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.7294548938104075,
+        "pitch": 0.0551071111937933,
+        "fov": 1.7835214743884122
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 3.114667846394804,
+          "pitch": -0.23106151046566126,
+          "title": "<i>L'istituzione dell'Eucarestia, circa 1720</i>",
+          "text": "Come tutti gli affreschi di questa chiesa, anche questo fu realizzato da Anton Domenico Bamberini.&nbsp;<div><br></div><div>Se vuoi saperne di più, puoi leggere il&nbsp;<b>testo n.6</b>.&nbsp;<div><br></div><div>Se preferisci, puoi anche ascoltarne la&nbsp;<b>versione audio&nbsp;</b>che si trova più in basso nella stessa pagina web.</div></div>"
+        },
+        {
+          "yaw": -2.229193903960976,
+          "pitch": -0.21265840132176272,
+          "title": "<i>La lavanda dei piedi, </i>circa 1720",
+          "text": "Come tutti gli affreschi di questa chiesa, anche questo fu realizzato da Anton Domenico Bamberini. Se vuoi saperne di più, puoi leggere il&nbsp;<b>testo n.6</b>.&nbsp;<div><br></div><div>Se preferisci, puoi anche ascoltarne la <b>versione audio </b>che si trova più in basso nella stessa pagina web.</div>"
+        },
+        {
+          "yaw": -1.276164953210463,
+          "pitch": -0.9100250022062948,
+          "title": "<i>Le virtù di Cristo</i>, circa 1720",
+          "text": "All'interno dei quattro medaglioni circolari posti al centro dei sottarchi sono rappresentate personificazioni delle&nbsp;<b><i>Virtù di Cristo</i>.</b>&nbsp;<div><br></div><div>Intorno ai medaglioni rotondi sono rappresentate, a coppie, vicende della <i><b>Via Crucis</b></i>.&nbsp;</div><div><br></div><div>Puoi trovare maggiori informazioni nel&nbsp;<b>testo n.5</b>: \"<b>Il programma iconografico della Chiesa del SS. Crocifisso</b>\".&nbsp;<div><br></div><div>Se preferisci, puoi anche ascoltarne la&nbsp;<b>versione audio&nbsp;</b>che si trova più in basso nella stessa pagina web.</div></div>"
+        },
+        {
+          "yaw": -1.265129590538157,
+          "pitch": -0.4270542161640023,
+          "title": "<i>Immacolata concezione di Maria, </i>olio su tela.",
+          "text": "Questa tela fu realizzata da Giovanbattista Lurchini nel XVIII sec.&nbsp;<div><b>Gli unici due dipinti che non furono realizzati dal Bamberini in questa chiesa</b> sono questo e l'altro che si trova sopra l'altare della parete opposta.</div>"
+        },
+        {
+          "yaw": 2.120349738020094,
+          "pitch": -0.4370680923325665,
+          "title": "<i>Il Transito di San Giuseppe,&nbsp;</i>&nbsp;olio su tela",
+          "text": "Questa tela fu realizzata da Giovanni Antonio Pucci nel 1730ca.&nbsp;&nbsp;<div><b>Questo dipinto e quello che si trova sulla parete opposta sono gli unici non realizzati da Anton Domenico Bamberini all'interno della chiesa.</b></div>"
+        },
+        {
+          "yaw": -2.6855428879845675,
+          "pitch": -0.6252857195340162,
+          "title": "<i>Il Santissimo Crocifisso</i>",
+          "text": "Il <b>Crocifisso</b> ligneo è custodito qui.&nbsp;<div><br></div><div>Se vuoi conoscere le vicende storiche del <b>miracoloso Crocifisso di Castelvecchio</b>, puoi leggere il <b>testo n. 1</b>.</div>"
+        },
+        {
+          "yaw": -2.6361261605098303,
+          "pitch": -1.2293421759282825,
+          "title": "<i>Ascensione di Cristo</i>, affresco della cupola",
+          "text": "L'<i>Ascensione di Cristo</i>, di Anton Domenico Bamberini (1720 ca.). Se vuoi saperne di più, puoi leggere il&nbsp;<b>testo n.7</b>.&nbsp;<div><br></div><div>Se preferisci, puoi anche ascoltarne la&nbsp;<b>versione audio&nbsp;</b>che si trova più in basso nella stessa pagina web.</div>"
+        },
+        {
+          "yaw": -0.557175664103827,
+          "pitch": -0.5857143741314896,
+          "title": "I Quattro Evangelisti",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.0998927745456122,
+          "pitch": -0.6430714343481228,
+          "title": "<i>Episodi della vita di Gesù,</i> affreschi a monocromo",
+          "text": "Sulle pareti dei quattro bracci della chiesa Bamberini dipinse sei grandi affreschi con altrettanti episodi della vita di Gesù: <i><b>La fuga in Egitto, L'entrata di Gesù a Gerusalemme, La lavanda dei piedi, L'Istituzione dell'Eucarestia.</b>&nbsp;</i><div><i><br></i></div><div><i>Per altre informazioni, puoi leggere il testo relativo o ascoltarlo in versione audio.</i></div>"
+        },
+        {
+          "yaw": 1.3778592869465456,
+          "pitch": -1.0895473024133686,
+          "title": "<i>I Quattro profeti maggiori</i>",
+          "text": "Ne pennacchi della cupola sono raffigurati i quattro profeti maggiori: <b>Isaia, Ezechiele, Geremia e Daniele</b><i>,</i> con i loro attributi tipici."
+        },
+        {
+          "yaw": 2.1393168662922246,
+          "pitch": -0.77600765770101,
+          "title": "<i>Gli otto profeti minori</i>",
+          "text": "Gli otto profeti \"minori\" sono dipinti a coppie ai lati di ciascuna finestra."
+        }
+      ]
+    }
+  ],
+  "name": "Il Santuario del Santissimo Crocifisso di San Miniato",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
